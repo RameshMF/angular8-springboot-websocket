aad1c27b6235f2168b8c54f9c673347690a0959a
Ramesh Fadatare <rfadatare@avaya.com>
2019-06-19 12:02:43 +0530

    spring boot angular websocket

---
+++ front-end/angular8-springboot-websocket/src/app/WebSocketAPI.ts
@@ -0,0 +1,54 @@
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import { AppComponent } from './app.component';
+
+export class WebSocketAPI {
+    webSocketEndPoint: string = 'http://localhost:8080/ws';
+    topic: string = "/topic/greetings";
+    stompClient: any;
+    appComponent: AppComponent;
+    constructor(appComponent: AppComponent){
+        this.appComponent = appComponent;
+    }
+    _connect() {
+        console.log("Initialize WebSocket Connection");
+        let ws = new SockJS(this.webSocketEndPoint);
+        this.stompClient = Stomp.over(ws);
+        const _this = this;
+        _this.stompClient.connect({}, function (frame) {
+            _this.stompClient.subscribe(_this.topic, function (sdkEvent) {
+                _this.onMessageReceived(sdkEvent);
+            });
+            //_this.stompClient.reconnect_delay = 2000;
+        }, this.errorCallBack);
+    };
+
+    _disconnect() {
+        if (this.stompClient !== null) {
+            this.stompClient.disconnect();
+        }
+        console.log("Disconnected");
+    }
+
+    // on error, schedule a reconnection attempt
+    errorCallBack(error) {
+        console.log("errorCallBack -> " + error)
+        setTimeout(() => {
+            this._connect();
+        }, 5000);
+    }
+
+	/**
+	 * Send message to sever via web socket
+	 * @param {*} message 
+	 */
+    _send(message) {
+        console.log("calling logout api via web socket");
+        this.stompClient.send("/app/hello", {}, JSON.stringify(message));
+    }
+
+    onMessageReceived(message) {
+        console.log("Message Recieved from Server :: " + message);
+        this.appComponent.handleMessage(JSON.stringify(message.body));
+    }
+}
---
+++ front-end/angular8-springboot-websocket/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { WebSocketAPI } from './WebSocketAPI';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular8-springboot-websocket';
+
+  webSocketAPI: WebSocketAPI;
+  greeting: any;
+  name: string;
+  ngOnInit() {
+    this.webSocketAPI = new WebSocketAPI(new AppComponent());
+  }
+
+  connect(){
+    this.webSocketAPI._connect();
+  }
+
+  disconnect(){
+    this.webSocketAPI._disconnect();
+  }
+
+  sendMessage(){
+    this.webSocketAPI._send(this.name);
+  }
+
+  handleMessage(message){
+    this.greeting = message;
+  }
+}
